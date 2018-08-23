a10cbf6abd86d4aa3188cdd3ed021d734627a43f
George Brande <george.brande@gmail.com>
2018-08-23 16:22:59 +0300

    express server init

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+require('./app/routes/customer.routes.js')(app);
+
+var server = app.listen(8081, function(){
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("app listenting at http://%s:%s",host, port)
+})
